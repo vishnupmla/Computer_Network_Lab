f9de85fc5adcf3b7d452594e72f59bddaf15243d
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-04-05 10:08:26 +0530

    Add files via upload

---
+++ loginpass.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+read -p "Enter the Username: " username
+read -sp "Enter the Password: " password
+
+
+
+if [[ $username == "user"  && $password == "password" ]]
+then
+  echo -e "\nYou're Logged In\n"
+elif [ $username != "user" ] 
+then
+  echo -e "\nInvalid User Name\n"
+else
+  echo -e "\nInvalid Password\n"
+fi
