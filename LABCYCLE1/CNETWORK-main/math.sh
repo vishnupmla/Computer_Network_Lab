bc09bfabf7c27479b8a38ad92a8b1482f46ebcfc
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-04-05 12:08:55 +0530

    Add files via upload

---
+++ LABCYCLE1/CNETWORK-main/math.sh
@@ -0,0 +1,13 @@
+echo Enter the number 
+ read a
+ echo Enter the number
+ read b
+ c=`expr $a + $b`
+ echo Addition= $c
+ d=`expr $a - $b`
+ echo subtraction=$b 
+ e=`expr $a \* $b`
+ echo Multipliccation=$e
+ f=`expr $a / $b`
+ echo Division=$f
+
